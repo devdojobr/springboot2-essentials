3ceab03514c09f8e04e1091eab234d045c527dc5
William Suane <william.suane@devdojo.academy>
2020-10-28 14:56:37 +0100

    feat: code used in video 27

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -2,17 +2,34 @@ package academy.devdojo.springboot2.client;
 
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
-        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class,2);
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(entity);
 
-        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class,2);
+        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
 
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+
+        log.info(Arrays.toString(animes));
+        //@formatter:off
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {});
+        //@formatter:on
+        log.info(exchange.getBody());
+
     }
 }
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -31,6 +31,12 @@ public class AnimeController {
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Anime>> listAll() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.listAllNonPageable());
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id) {
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
