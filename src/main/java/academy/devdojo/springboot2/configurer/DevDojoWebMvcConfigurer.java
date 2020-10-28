d5408ded5b50a71cd5ab7f30631fb44e5e0ac998
William Suane <william.suane@devdojo.academy>
2020-10-28 14:21:42 +0100

    feat: code used in video 25

--- src/main/java/academy/devdojo/springboot2/configurer/DevDojoWebMvcConfigurer.java
+++ src/main/java/academy/devdojo/springboot2/configurer/DevDojoWebMvcConfigurer.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class DevDojoWebMvnConfigurer implements WebMvcConfigurer {
+public class DevDojoWebMvcConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
