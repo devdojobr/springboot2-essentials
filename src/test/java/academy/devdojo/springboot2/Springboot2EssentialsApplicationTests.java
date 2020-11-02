4fb60430921a65276583fc45b1548e7d015c8649
William Suane <william.suane@devdojo.academy>
2020-11-02 20:04:10 +0100

    feat: code used in video 38

--- src/test/java/academy/devdojo/springboot2/Springboot2EssentialsApplicationTests.java
+++ src/test/java/academy/devdojo/springboot2/Springboot2EssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package academy.devdojo.springboot2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Springboot2EssentialsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
