10dd4cbebcd364ee52bec2929d608c58b6a8412f
William Suane <william.suane@devdojo.academy>
2020-11-03 19:47:03 +0100

    feat: code used in video 40

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -14,7 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+//                csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
