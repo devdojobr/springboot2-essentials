8f6d7b293857fd1d79df237ead1b63af8f98daeb
William Suane <william.suane@devdojo.academy>
2020-11-06 20:34:31 +0100

    feat: code used in video 50

--- src/main/java/academy/devdojo/springboot2/Springboot2EssentialsApplication.java
+++ src/main/java/academy/devdojo/springboot2/Springboot2EssentialsApplication.java
@@ -1,7 +1,11 @@
 package academy.devdojo.springboot2;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Springboot2EssentialsApplication {
@@ -10,4 +14,9 @@ public class Springboot2EssentialsApplication {
 		SpringApplication.run(Springboot2EssentialsApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
