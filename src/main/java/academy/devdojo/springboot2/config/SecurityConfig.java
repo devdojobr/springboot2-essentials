6a8a1e482b94c2055d9fd4c6ecc75080bfe6c159
William Suane <william.suane@devdojo.academy>
2020-11-06 19:39:47 +0100

    feat: code used in video 48

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/animes/admin/**").hasRole("ADMIN")
                 .antMatchers("/animes/**").hasRole("USER")
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
