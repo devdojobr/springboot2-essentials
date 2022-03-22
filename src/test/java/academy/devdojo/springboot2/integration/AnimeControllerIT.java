cf7177b32cf44f734f8b8db3fbfbb2911d56fa0a
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-03-22 13:42:16 -0300

    Aula 36 - Testes de Integração pt 01

---
+++ src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
@@ -0,0 +1,55 @@
+package academy.devdojo.springboot2.integration;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.repository.AnimeRepository;
+import academy.devdojo.springboot2.util.AnimeCreator;
+import academy.devdojo.springboot2.wrapper.PageableResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//sempre cria uma porta aleatoria, para nao termos que usar a porta que está em produção
+@AutoConfigureTestDatabase
+//Com essa anotacao nossa configuracao de banco de dados vai utilizar o valor que temos em memoria
+class AnimeControllerIT {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    //    @LocalServerPort
+//    private int port;  //para pegar uma porta expecifica passada
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("List returns list of anime inside page object when successful")
+    void list_ReturnsListOfAnimesInsidePageObject_WhenSuccessful() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createAnimeToBeSaved());
+
+        String expectedName = savedAnime.getName();
+
+        PageableResponse<Anime> animePage = testRestTemplate.exchange("/animes", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Anime>>() {
+                }).getBody();
+
+        Assertions.assertThat(animePage).isNotNull();
+
+        Assertions.assertThat(animePage.toList())
+                .isNotEmpty()
+                .hasSize(1);
+
+        Assertions.assertThat(animePage.toList().get(0).getName()).isEqualTo(expectedName);
+
+    }
+
+
+}
